aa20ff389decd598de9eb4c82b3512baf526bc57
Betatech768 <gudzy86@gmail.com>
2026-07-21 18:18:31 +0100

    added split image and link

--- public/src/split_delimiter.py
+++ public/src/split_delimiter.py
@@ -36,24 +36,51 @@ def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             new_node.append(old_node)
+            continue
         text = old_node.text
 
         image_node_list: list[tuple[str, str]] = extract_markdown_images(text)
-
+        if len(image_node_list) == 0:
+            new_node.append(old_node)
         for image in image_node_list:
             image_alt = image[0]
             image_link = image[1]
-            section = old_node.text.split(f"![{image_alt}]({image_link})", 1)
+            section = text.split(f"![{image_alt}]({image_link})", 1)
             if len(section) != 2:
                 raise Exception("invalid markdown, image section not closed")
             if section[0] != "":
                 new_node.append(TextNode(section[0], TextType.TEXT))
             new_node.append(TextNode(image_alt, TextType.IMAGE, image_link))
-            old_nodes = section[1]
+            text = section[1]
         if text != "":
             new_node.append(TextNode(text, TextType.TEXT))
     return new_node
 
 
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
-    pass
+    new_node: list[TextNode] = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_node.append(old_node)
+            continue
+        text = old_node.text
+
+        link_node_list: list[tuple[str, str]] = extract_markdown_links(text)
+        if len(link_node_list) == 0:
+            new_node.append(old_node)
+        for link in link_node_list:
+            link_description: str = link[0]
+            link_address: str = link[1]
+
+            section = text.split(f"[{link_description}]({link_address})", 1)
+            if len(section) != 2:
+                raise Exception("invalid markdown, link section not closed")
+
+            if section[0] != "":
+                new_node.append(TextNode(section[0], TextType.TEXT))
+            new_node.append(TextNode(link_description, TextType.LINK, link_address))
+            text = section[1]
+        if text != "":
+            new_node.append(TextNode(text, TextType.TEXT))
+    return new_node
--- public/src/test_split_delimiter.py
+++ public/src/test_split_delimiter.py
@@ -1,7 +1,7 @@
 import unittest
 from textnode import TextType, TextNode
-from split_delimiter import split_nodes_delimiter
-
+from split_delimiter import split_nodes_delimiter, split_nodes_image, split_nodes_link
+from text_to_textnode import text_to_textnode
 
 class TestSplitDelimiter(unittest.TestCase):
     def test_split_delimiter(self):
@@ -17,6 +17,45 @@ class TestSplitDelimiter(unittest.TestCase):
 
         self.assertEqual(node, node2)
 
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+    def test_text_to_textnode(self):
+        new_nodes = text_to_textnode("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        self.assertListEqual(
+                    [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ], new_nodes,
+        )
 
 if __name__ == "__main__":
     unittest.main()
+
+
+
+
+
